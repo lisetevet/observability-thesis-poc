da92dc889c44d380ca24e985e379d18c60b8c2ba
lissu <livete@taltech.ee>
2026-04-06 21:50:00 +0300

    fix(profile-service): improve Mongo repo error handling and logging

--- services/profile-service/repository/mongo_profile_repository.go
+++ services/profile-service/repository/mongo_profile_repository.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"context"
+	"errors"
+    "log"
 
 	"profile-service/model"
 
@@ -18,20 +20,24 @@ func NewMongoProfileRepository(coll *mongo.Collection) *MongoProfileRepository {
 }
 
 func (r *MongoProfileRepository) GetByUUID(ctx context.Context, uuid string) (Profile, bool, error) {
-	var doc model.Profile
-	err := r.coll.FindOne(ctx, bson.M{"uuid": uuid}).Decode(&doc)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return Profile{}, false, nil
-		}
-		return Profile{}, false, err
-	}
+    var doc model.Profile
+    err := r.coll.FindOne(ctx, bson.M{"uuid": uuid}).Decode(&doc)
 
-	return Profile{
-		UUID:         doc.UUID,
-		Name:         doc.Name,
-		Surname:      doc.Surname,
-		Email:        doc.Email,
-		PersonalCode: doc.PersonalCode,
-	}, true, nil
+    if err == nil {
+        return Profile{
+            UUID:         doc.UUID,
+            Name:         doc.Name,
+            Surname:      doc.Surname,
+            Email:        doc.Email,
+            PersonalCode: doc.PersonalCode,
+        }, true, nil
+    }
+
+    if errors.Is(err, mongo.ErrNoDocuments) {
+		log.Printf("No documents found.")
+        return Profile{}, false, nil
+    }
+
+    log.Printf("mongo GetByUUID failed (uuid=%s): %v", uuid, err)
+    return Profile{}, false, err
 }
